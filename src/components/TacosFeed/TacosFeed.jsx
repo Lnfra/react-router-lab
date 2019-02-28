685795ab3fee6a49e09922019459cf28735fc20f
Angeline <atan@thoughtworks.com>
2019-02-28 11:03:10 +0800

    Exploring history object

--- src/components/TacosFeed/TacosFeed.jsx
+++ src/components/TacosFeed/TacosFeed.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-function TacosFeed({ match }) {
+function TacosFeed({ match, history }) {
+  const onClickBack = () => { history.push("/")}
   return (
     <div>
       <h1>This is your TacosFeed</h1>
       <h2>{match.params.id}</h2>
+      <button onClick={onClickBack}>Back</button>
     </div>
+    
   );
 }
 
